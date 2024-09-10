c001371946f2ef0041e2299c4ff752ac6f12a2f7
Eddie <edwardchen_97@hotmail.com>
2024-09-10 15:43:49 +1000

    added calendar model

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -10,48 +10,10 @@ const { format, addDays, addWeeks, startOfWeek } = require("date-fns");
 
 //* checking dates to display below the days
 
-router.get("/", (req, res) => {
 
-//this gets today's date (10/09/2024)
-const today = new Date();
 
-// this makes sure the start of the week is Monday (09/09/2024)
-// Default is Sunday
-const startOfTheWeek = startOfWeek(today, { weekStartsOn: 1 });
 
-let monday = addDays(startOfTheWeek, 0);
-let formatMonday = format(monday, "dd-MM-yyyy");
-
-let tuesday = addDays(startOfTheWeek, 1);
-let formatTuesday = format(tuesday, "dd-MM-yyyy");
-
-let wednesday = addDays(startOfTheWeek, 2);
-let formatWednesday = format(wednesday, "dd-MM-yyyy");
-
-let thursday = addDays(startOfTheWeek, 3);
-let formatThursday = format(thursday, "dd-MM-yyyy");
-
-let friday = addDays(startOfTheWeek, 4);
-let formatFriday = format(friday, "dd-MM-yyyy");
-
-let saturday = addDays(startOfTheWeek, 5);
-let formatSaturday = format(saturday, "dd-MM-yyyy");
-
-let sunday = addDays(startOfTheWeek, 6);
-let formatSunday = format(sunday, "dd-MM-yyyy");
-
-const formatDates = {
-formatMonday,
-formatTuesday,
-formatWednesday,
-formatThursday,
-formatFriday,
-formatSaturday,
-formatSunday,
-};
-//* all Dates correct and formatted correctly
-
-res.render('calendar/weekly.ejs', {formatDates})
+//* show selected day
 
 
 
@@ -59,7 +21,57 @@ res.render('calendar/weekly.ejs', {formatDates})
 
 
 
-});
+
+
+//! not sure what to do with this code now 
+// router.get("/", (req, res) => {
+
+// //this gets today's date (10/09/2024)
+// const today = new Date();
+
+// // this makes sure the start of the week is Monday (09/09/2024)
+// // Default is Sunday
+// const startOfTheWeek = startOfWeek(today, { weekStartsOn: 1 });
+
+// let monday = addDays(startOfTheWeek, 0);
+// let formatMonday = format(monday, "dd-MM-yyyy");
+
+// let tuesday = addDays(startOfTheWeek, 1);
+// let formatTuesday = format(tuesday, "dd-MM-yyyy");
+
+// let wednesday = addDays(startOfTheWeek, 2);
+// let formatWednesday = format(wednesday, "dd-MM-yyyy");
+
+// let thursday = addDays(startOfTheWeek, 3);
+// let formatThursday = format(thursday, "dd-MM-yyyy");
+
+// let friday = addDays(startOfTheWeek, 4);
+// let formatFriday = format(friday, "dd-MM-yyyy");
+
+// let saturday = addDays(startOfTheWeek, 5);
+// let formatSaturday = format(saturday, "dd-MM-yyyy");
+
+// let sunday = addDays(startOfTheWeek, 6);
+// let formatSunday = format(sunday, "dd-MM-yyyy");
+
+// const formatDates = {
+// formatMonday,
+// formatTuesday,
+// formatWednesday,
+// formatThursday,
+// formatFriday,
+// formatSaturday,
+// formatSunday,
+// };
+// //* all Dates correct and formatted correctly
+
+// res.render('calendar/weekly.ejs', {formatDates})
+
+
+
+// });
+
+
 
 
 
---
+++ models/Calendar.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const calendarSchema = new mongoose.Schema(
+{
+    title:{
+        type: String, 
+        required: true
+    },
+
+    date: {
+        type: String, 
+        required: true 
+    }, 
+    owner: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+
+    }
+
+
+
+}, 
+{timestamps: true}
+);
+
+const Calendar = mongoose.model("Calendar" , calendarSchema);
+
+module.exports = Calendar
